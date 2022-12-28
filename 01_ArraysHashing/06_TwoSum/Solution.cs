2dd827d12c87410bbf54de44792d2899c4e29940
Daniel Praźmo <d.prazmo@icloud.com>
2022-12-28 20:30:19 +0100

    Add new problem: ArraysHashing/TwoSum.

--- 01_ArraysHashing/06_TwoSum/Solution.cs
+++ 01_ArraysHashing/06_TwoSum/Solution.cs
@@ -1,6 +1,56 @@
-namespace NeetCodeAllCSharp._01_ArraysHashing._06_TwoSum;
+/*
+ * https://leetcode.com/problems/two-sum/description/
+ *
+ * Given an array of integers `nums` and an integer `target`, return _indices of
+ * the two numbers such that they add up to `target`_.
+ *
+ * You may assume that each input would have *_exactly_ one solution*,
+ * and you may not use the _same_ element twice.
+ *
+ * You can return the answer in any order.
+ *
+ * Example 1:
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ *
+ * Example 2:
+ * Input: nums = [3,2,4], target = 6
+ * Output: [1,2]
+ *
+ * Example 3:
+ * Input: nums = [3,3], target = 6
+ * Output: [0,1]
+ * 
+ * Constraints:
+ * - `2 <= nums.length <= 10^4`
+ * - `-10^9 <= nums[i] <= 10^9`
+ * - `-10^9 <= target <= 10^9`
+ * - *Only one valid answer exists.*
+ */
 
-public class Solution
+// ReSharper disable once CheckNamespace
+namespace NeetCodeAllCSharp.ArraysHashing.TwoSum;
+
+public interface ISolution
 {
-    
+    int[] TwoSum(int[] nums, int target);
+}
+
+public class Solution : ISolution
+{
+    // OJ score: 148 ms, 44.3 MB
+    public int[] TwoSum(int[] nums, int target) {
+        var dic = new Dictionary<int,int>();
+        for(var i=0; i < nums.Length; ++i){
+            var rest = target - nums[i];
+            
+            if(dic.ContainsKey(rest))
+                return new[]{i, dic[rest]};
+            
+            if(!dic.ContainsKey(nums[i]))
+                dic.Add(nums[i], i);
+        }
+        return new int[2];
+    }
 }
--- 01_ArraysHashing/06_TwoSum/Test.cs
+++ 01_ArraysHashing/06_TwoSum/Test.cs
@@ -1,6 +1,71 @@
+/*
+ * https://leetcode.com/problems/two-sum/description/
+ *
+ * Given an array of integers `nums` and an integer `target`, return _indices of
+ * the two numbers such that they add up to `target`_.
+ *
+ * You may assume that each input would have *_exactly_ one solution*,
+ * and you may not use the _same_ element twice.
+ *
+ * You can return the answer in any order.
+ *
+ * Example 1:
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ *
+ * Example 2:
+ * Input: nums = [3,2,4], target = 6
+ * Output: [1,2]
+ *
+ * Example 3:
+ * Input: nums = [3,3], target = 6
+ * Output: [0,1]
+ * 
+ * Constraints:
+ * - `2 <= nums.length <= 10^4`
+ * - `-10^9 <= nums[i] <= 10^9`
+ * - `-10^9 <= target <= 10^9`
+ * - *Only one valid answer exists.*
+ */
+
+// ReSharper disable once CheckNamespace
 namespace NeetCodeAllCSharp.ArraysHashing.TwoSum;
 
-public class Test
+internal class TestCase
 {
+    public bool Run(ISolution solution)
+    {
+        var result = solution.TwoSum(Nums, Target);
+        Array.Sort(result);
+        Array.Sort(Expected);
+        return result.SequenceEqual(Expected);
+    }
     
+    public int[] Nums { get; init; } = null!;
+    public int Target { get; init; }
+    public int[] Expected { get; init; } = null!;
+}
+
+public class Test : ITest
+{
+    public Test(ISolution solution)
+    {
+        _solution = solution;
+        _testCases = new List<TestCase>
+        {
+            new() { Nums = new[] { 2, 7, 11, 15 }, Target = 9, Expected = new[] { 0, 1 } },
+            new() { Nums = new[] { 3, 2, 4 }, Target = 6, Expected = new[] { 1, 2 } },
+            new() { Nums = new[] { 3, 3 }, Target = 6, Expected = new[] { 0, 1 } }
+        };
+    }
+    public Test() : this(new Solution()) { }
+    
+    public bool Run()
+    {
+        return _testCases.All(testCase => testCase.Run(_solution));
+    }
+    
+    private readonly ISolution _solution;
+    private readonly List<TestCase> _testCases;
 }
