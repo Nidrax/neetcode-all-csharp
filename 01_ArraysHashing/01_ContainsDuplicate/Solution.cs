5eaae034cc84f3659aba0d503091081e5b5c6173
Daniel Praźmo <d.prazmo@icloud.com>
2022-12-28 19:19:35 +0100

    Update problem description.

--- 01_ArraysHashing/01_ContainsDuplicate/Solution.cs
+++ 01_ArraysHashing/01_ContainsDuplicate/Solution.cs
@@ -1,3 +1,26 @@
+/*
+ * https://leetcode.com/problems/contains-duplicate/description/
+ * 
+ * Given an integer array `nums`, return `true` if any value appears *at least
+ * twice* in the array, and return `false` if every element is distinct.
+ *
+ * Example 1:
+ * Input: nums = [1,2,3,1]
+ * Output: true
+ *
+ * Example 2:
+ * Input: nums = [1,2,3,4]
+ * Output: false
+ *
+ * Example 3:
+ * Input: nums = [1,1,1,3,3,4,3,2,4,2]
+ * Output: true
+ *
+ * Constraints:
+ * - `1 <= nums.length <= 10^5`
+ * - `-10^9 <= nums[i] <= 10^9`
+ */
+
 // ReSharper disable once CheckNamespace
 namespace NeetCodeAllCSharp.ArraysHashing.ContainsDuplicate;
 
@@ -8,6 +31,7 @@ public interface ISolution
 
 public class Solution : ISolution
 {
+    //OJ score: 176 ms, 52.7 MB
     public bool ContainsDuplicate(int[] nums) {
         var dic = new HashSet<int>();
         foreach(var num in nums)
